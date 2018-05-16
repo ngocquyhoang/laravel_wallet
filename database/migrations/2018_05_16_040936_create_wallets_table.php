63e00fbec456b4b59129380ff1179b4f42cc2bca
ngocquyhoang <ngocquyhoang.93@gmail.com>
2018-05-16 17:49:41 +0700

    Initialize Laravel and create Model

---
+++ database/migrations/2018_05_16_040936_create_wallets_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWalletsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wallets', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->string('name')->unique();
+            $table->unsignedBigInteger('balance');
+            $table->unsignedInteger('currency');
+            $table->unsignedInteger('type');
+            $table->text('comment')->nullable();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');;
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wallets');
+    }
+}
---
+++ database/migrations/2018_05_16_040938_create_transactions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('wallet_id');
+            $table->unsignedInteger('category_id');
+            $table->unsignedBigInteger('amount');
+            $table->text('comment')->nullable();
+            $table->dateTime('transaction_time');
+            $table->timestamps();
+
+            $table->foreign('wallet_id')->references('id')->on('wallets')->onDelete('cascade');;
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');;
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
